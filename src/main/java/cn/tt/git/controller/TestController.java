b69a18fe076b84193c1e030a92df78764319751f
H <tt_H@163.com>
2022-04-01 10:47:56 +0800

    github

--- src/main/java/cn/tt/git/controller/TestController.java
+++ src/main/java/cn/tt/git/controller/TestController.java
@@ -4,7 +4,7 @@ public class TestController {
     public static void main(String[] args){
         System.out.println("hello git!");
         System.out.println("hello git! dev 分支");
-
         System.out.println("hello2 git!");
+        System.out.println("这是github第一次提交");
     }
 }
