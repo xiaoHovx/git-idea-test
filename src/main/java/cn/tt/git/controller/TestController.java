14759dd73ea2b88d3dfd694f323262aecce0be4b
xiaoHovx <102583003+xiaoHovx@users.noreply.github.com>
2022-04-01 11:16:43 +0800

    Update TestController.java

--- src/main/java/cn/tt/git/controller/TestController.java
+++ src/main/java/cn/tt/git/controller/TestController.java
@@ -6,7 +6,6 @@ public class TestController {
         System.out.println("hello git! dev 分支");
         System.out.println("hello2 git!");
         System.out.println("这是github第一次提交");
-        System.out.println("这是github第一次提交");
-        System.out.println("线上更改")
+        System.out.println("线上更改");
     }
 }
